63245138d77149fafb10a77b3a6670472ec91776
gnazarkin <gleb.nazarkin@gmail.com>
2014-06-03 18:08:43 -0400

    Defined method for outputing list, added rice to the list & outputs length of the list

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,15 @@
 grocery_list = ["carrots", "toilet paper","apples","salmon"]
-grocery_list.each do| item|
-	puts "* #{item}"
+
+def output_list (list)
+	list.each do |item|
+		puts "* #{item}"
 	end
+end
+
+output_list(grocery_list)
+
+grocery_list << "rice"
+
+output_list(grocery_list)
+
+puts grocery_list.length
